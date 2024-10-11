d7b8d030864114ee663fa7cb957f6fd487ad7d13
unknown <mdamir4298@gmail.com>
2024-10-11 18:02:10 +0530

    navbar completed using css

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,10 @@
 // import React from 'react'
-
+import "./index.css"
 import Navbar from "./components/Navbar/Navbar"
 
 const App = () => {
   return (
-    <div>
+    <div className="app">
       <Navbar/>
     </div>
   )
--- frontend/src/components/Navbar/Navbar.jsx
+++ frontend/src/components/Navbar/Navbar.jsx
@@ -1,11 +1,36 @@
 // import React from 'react'
+import "./Navbar.css"
+import { assets } from "../../assets/frontend_assets/assets";
+import { useState } from "react";
 
 const Navbar = () => {
+    const [menu,setMenu]=useState("home")
+
+    
+
+    
   return (
     <div className="navbar">
-      
-    </div>
-  )
-}
+      <img src={assets.logo} alt="" className="logo" />
 
-export default Navbar
+      <ul className="navbar_menu">
+        <li onClick={()=>setMenu("home")} className={menu==="home"?"active":""}>home</li>
+        <li onClick={()=>setMenu("menu")} className={menu==="menu"?"active":""}>menu</li>
+        <li onClick={()=>setMenu("mobile-app")} className={menu==="mobile-app"?"active":""}>mobile-app</li>
+        <li onClick={()=>setMenu("contact-us")} className={menu==="contact-us"?"active":""}>contact us</li>
+      </ul>
+
+      <div className="navbar_right">
+        <img src={assets.search_icon} alt="" />
+        <div className="navbar_search_icon">
+          <img src={assets.basket_icon} alt="" />
+          <div className="dot"></div>
+        </div>
+        <button>sign in</button>
+      </div>
+
+    </div>
+  );
+};
+
+export default Navbar;
