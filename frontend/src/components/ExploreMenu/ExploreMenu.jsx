bb9c0cfca3c2b701fe19d7eca610887e60cdb197
unknown <mdamir4298@gmail.com>
2024-10-11 19:22:15 +0530

    menu list added

--- frontend/src/components/ExploreMenu/ExploreMenu.jsx
+++ frontend/src/components/ExploreMenu/ExploreMenu.jsx
@@ -1,11 +1,28 @@
 // import React from 'react'
-import "./ExploreMenu.css"
+import "./ExploreMenu.css";
+import { menu_list } from "../../assets/frontend_assets/assets";
+
 const ExploreMenu = () => {
   return (
-    <div>
-      
+    <div className="explore_menu" id="explore_menu_id">
+      <h1>Explore our menu</h1>
+      <p className="explore_menu_text">
+        {" "}
+        Choose from a diverse menu featuring a delectable array of dishes
+        crafted with the fitness ingredients and elevate your dining
+        experience,one delicious meal at a time.
+      </p>
+      <div className="explore_menu_list">
+        {menu_list.map((elem, i) => (
+          <div className="explore_menu_list_item" key={i}>
+            <img src={elem.menu_image} alt="" />
+            <p>{elem.menu_name}</p>
+          </div>
+        ))}
+      </div>
+      <hr />
     </div>
-  )
-}
+  );
+};
 
-export default ExploreMenu
+export default ExploreMenu;
--- frontend/src/pages/Home/Home.jsx
+++ frontend/src/pages/Home/Home.jsx
@@ -1,12 +1,14 @@
 // import React from 'react'
+import ExploreMenu from "../../components/ExploreMenu/ExploreMenu"
 import Header from "../../components/Header/Header"
 import "./Home.css"
 const Home = () => {
   return (
     <div>
       <Header/>
+      <ExploreMenu/>
     </div>
   )
-}
+} 
 
 export default Home
